e9b13e3a344eae296b7fa8ff019f2132556aa09b
blthree <blthree89@gmail.com>
2017-05-01 19:43:48 -0400

    new SALK T-DNA data

--- SALK_finder.py
+++ SALK_finder.py
@@ -3,40 +3,13 @@ from tinydb import TinyDB, Query
 import requests
 '''This script will can build and query a database of T-DNA insertions, built from the GBrowse track on TAIR'''
 
-data_filename = 'data/tDNAs.gff3'
+data_filename = 'data/T-DNA.salk.tab'
 
+df = pd.read_csv(data_filename, sep='\t', header=None)
+df = pd.concat([df, df[3].str.split('/', expand=True)], axis=1)
 
-def create_db(filename, db_filename):
-    db = TinyDB('tdna_db.json')
-    df = pd.read_csv(filename, sep='\t', header=None, comment='#')
-    df.drop([1, 2, 5, 6, 7], axis=1, inplace=True)
-    df.columns = ['chr', 'start', 'end', 'info']
-    df['name'] = df['info'].str.split(';', expand=True)[0]
-    df.drop(['info'], axis=1, inplace=True)
-    df.replace(['Name=', 'Chr'], '', inplace=True, regex=True)
-    #df.set_index('name', drop=True, inplace=True)
-    jason = df.to_json(orient='records')
-    savef = open(db_filename, 'w')
-    savef.write(jason)
-    return
-#create_db(data_filename, 'db/tdna_db.json')
-# load the database
-#db = TinyDB('db/testdb.json')
-#db.insert({'name':'SALK_111110', 'chr': '2', 'start': '234', 'end': '243'})
-#new_search = Query()
-#c = db.search(new_search.name == 'GK-887B01-030271')
-#print(db.all())
-
-url = 'http://www.arabidopsis.org/servlets/TairObject?name=SALK_105972.55.50.X&type=polyallele'
-url2 = 'http://www.arabidopsis.org/servlets/TairObject?name=SALK_069069.23.60.x&type=polyallele'
-r = requests.get(url2)
-
-find_string = 'nuc_sequence</td>\n                <td class="sm">'
-i_start = r.text.index('nuc_sequence</td>\n                <td class="sm">')
-i_start += len(find_string) + 1
-i_end = r.text[i_start:i_start+200].index('</td>')
-print(i_end)
-print(r.text[i_start:i_start+i_end])
-#print(r.text)
-#nuc_sequence</td>
-#                <td class="sm">
+df.columns = ['name', 'chr', 'eval', 'delete_this', 'orientation', 'coords']
+df.drop(['eval', 'delete_this'], axis=1, inplace=True)
+temp = df['chr'].str.split(':')
+df['chr'] = temp.str[0]
+print(df.head())
