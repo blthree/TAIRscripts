7a0bd0abf614f3f264aa03a51970a37c33c19ccd
blthree <blthree89@gmail.com>
2017-05-25 17:46:04 -0400

    refactoring SALK_finder

--- SALK_finder.py
+++ SALK_finder.py
@@ -1,15 +1,31 @@
-import pandas as pd
-from tinydb import TinyDB, Query
-import requests
-'''This script will can build and query a database of T-DNA insertions, built from the GBrowse track on TAIR'''
+from linecache import getline
+import primer3
 
 data_filename = 'data/T-DNA.salk.tab'
 
-df = pd.read_csv(data_filename, sep='\t', header=None)
-df = pd.concat([df, df[3].str.split('/', expand=True)], axis=1)
+def load_data(filename):
+    f = open(filename, 'r')
+    records = {}
+    for line in f:
+        s_line = line.strip('\n').split('\t')
+        poly_name = s_line[0]
+        poly_chr, poly_loc = s_line[1].split(':')
+        poly_chr = poly_chr[3:]
+        poly_loc = int(poly_loc)
+        orientation = s_line[3].split('/')[0]
+        # load into dict of dicts object
+        records[poly_name] = {'name':poly_name, 'chr':poly_chr, 'orientation':orientation, 'location':poly_loc}
+    return records
 
-df.columns = ['name', 'chr', 'eval', 'delete_this', 'orientation', 'coords']
-df.drop(['eval', 'delete_this'], axis=1, inplace=True)
-temp = df['chr'].str.split(':')
-df['chr'] = temp.str[0]
-print(df.head())
+def extract_sequence(chr, location, upstream_dist=0, downstream_dist=0):
+    # SALK_058673.44.55.x	chr2:011164117	1e-18	W/11164117-11164162
+    filename = 'chr' + str(chr) + '.fa'
+    # calculate line number, add one line since fasta file has a header
+    # and another line to account for zero-indexed numbering of linecache.getline()
+    line_no = int(location / 60) + 2
+    # calculate the exact location of the insertion on the line
+    char_no = location % 60
+    result = getline(filename, line_no).strip('\n')
+    return result
+
+extract_sequence(2,11164117)
