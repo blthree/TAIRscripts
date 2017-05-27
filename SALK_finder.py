c84aa7e4284bb5aec955e81175a27511324df4be
blthree <blthree89@gmail.com>
2017-05-26 20:13:38 -0400

    use pyfaidx for fasta lookup

--- SALK_finder.py
+++ SALK_finder.py
@@ -1,7 +1,9 @@
-from linecache import getline
-import primer3
+from pyfaidx import Fasta
+from primer3.bindings import designPrimers
 
 data_filename = 'data/T-DNA.salk.tab'
+# http://signal.salk.edu/database/transcriptome/AT9.fa
+# http://signal.salk.edu/database/transcriptome/T-DNA.SALK
 
 def load_data(filename):
     f = open(filename, 'r')
@@ -9,23 +11,33 @@ def load_data(filename):
     for line in f:
         s_line = line.strip('\n').split('\t')
         poly_name = s_line[0]
-        poly_chr, poly_loc = s_line[1].split(':')
+        poly_chr = s_line[1].split(':')[0]
         poly_chr = poly_chr[3:]
-        poly_loc = int(poly_loc)
+        poly_start = s_line[3].split('/')[1].split('-')[0]
+        poly_end = s_line[3].split('/')[1].split('-')[1]
         orientation = s_line[3].split('/')[0]
         # load into dict of dicts object
-        records[poly_name] = {'name':poly_name, 'chr':poly_chr, 'orientation':orientation, 'location':poly_loc}
+        records[poly_name] = {'name':poly_name, 'chr':poly_chr, 'orientation':orientation, 'start':poly_start, 'end':poly_end}
     return records
 
-def extract_sequence(chr, location, upstream_dist=0, downstream_dist=0):
-    # SALK_058673.44.55.x	chr2:011164117	1e-18	W/11164117-11164162
-    filename = 'chr' + str(chr) + '.fa'
-    # calculate line number, add one line since fasta file has a header
-    # and another line to account for zero-indexed numbering of linecache.getline()
-    line_no = int(location / 60) + 2
-    # calculate the exact location of the insertion on the line
-    char_no = location % 60
-    result = getline(filename, line_no).strip('\n')
-    return result
 
-extract_sequence(2,11164117)
+
+db = load_data(data_filename)
+print(db.get('SALK_147584.18.15.x'))
+genome = Fasta('AT9.fa')
+print(genome.keys())
+info = db.get('SALK_147584.18.15.x')
+name = 'chr' + info['chr']
+print(name)
+print(genome[name][int(info['start']):int(info['end'])])
+# still need to revcomp if reverse strand!
+
+'''default iSECT values:
+size: optimal=21 min=18 max=28
+Tm: opt=61 Min=53 Max=71
+%GC: min=20 max=80
+clamp=1
+maxN=300 ext5=300 ext3=300
+primer_zone=200
+BPos=110 (distance from LB primer to insertion site)
+'''
